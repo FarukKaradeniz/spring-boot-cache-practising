d62eb9ac8e10dcaa49c1a55e3ed986cbbe3dd216
Ömer Faruk Karadeniz <krdnzomer@gmail.com>
2022-08-14 23:38:49 +0300

    init
    Signed-off-by: Ömer Faruk Karadeniz <krdnzomer@gmail.com>

---
+++ src/main/java/com/farukkaradeniz/cachepractising/CachePractisingApplication.java
@@ -0,0 +1,13 @@
+package com.farukkaradeniz.cachepractising;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachePractisingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CachePractisingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/farukkaradeniz/cachepractising/CachePractisingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farukkaradeniz.cachepractising;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachePractisingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
